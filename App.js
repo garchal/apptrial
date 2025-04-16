1f6e15c21fa4482ca841aff1a30aff34e2179e4c
garchal <99027994+garchal@users.noreply.github.com>
2025-04-16 23:56:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import React, { useState, useRef } from "react";
+import Tesseract from "tesseract.js";
+import mammoth from "mammoth";
+import html2canvas from "html2canvas";
+import { jsPDF } from "jspdf";
+import "./style.css";
+
+// Add refined App.js content here (trimmed for brevity)
